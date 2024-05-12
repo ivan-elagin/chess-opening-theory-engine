28da371b3edb0a444238ffd05a17ae7de10a4adf
ivan-elagin <ivan-elagin@users.noreply.github.com>
2024-05-12 18:23:59 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+    OPENING THEORY TOOL
+    -------------------
+    
+    The goal of this project is to provide an efficient and helpful tool for learning chess openings theory.
+    A chess opening is a sequence of moves that is played at the beginning of a game. 
+    It is usually a good idea to follow a well-known opening because it has been studied and played by many people before. 
+    This allows you to avoid traps and to reach a good position in the middlegame.
+    
+    The user can select the opening they want to learn and the tool with provide them with the moves
+    and comments for each move. The user can also play the moves on the board and see the evaluation of the position.
+    
+    I hope this tool can help beginners and intermediate players to learn and understand the openings better. Try it out and enjoy the game!
+    
+"""
+
